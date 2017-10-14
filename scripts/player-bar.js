ef89501dec489a6fc9314fd2e8eaca522ad19b1b
Robert Marsh <nhe6dc@gmail.com>
2017-10-14 16:50:27 -0400

    volume button working

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -16,13 +16,39 @@ $( document ).ready(function() {
   });
   $("button#previous").click( function() {
 
-      if (player.playState !== 'playing') { return; }
-      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-      const previousSongIndex = currentSongIndex - 1;
-      if (previousSongIndex >= album.songs.length) { return; }
-      const previousSong = album.songs[previousSongIndex];
-      player.playPause(previousSong);
+    if (player.playState !== 'playing') { return; }
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+    if (previousSongIndex >= album.songs.length) { return; }
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
 
-      });
+  });
+
+  $('#time-control input').on('input', function (event) {
+    player.skipTo(event.target.value);
+  });
+
+  setInterval( () => {
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+  }, 1000);
+
+$("#volume-control input").on("input", function (event){
+  player.setVolume(event.target.value);
+});
+
+setInterval(()=> {
+  const currentVolume = player.getVolume();
+  const max = player.getMax();
+  const percent = (currentVolume / max) * 100;
+  $("#volume-control .current-Volume").text( currentVolume)
+  $("#volume-control input").val(percent);
+}, 1000);
+
+
 
 });
